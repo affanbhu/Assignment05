304b25f1d4b5f8e2d52375ba51a7505d4f24c053
AffanBhuiyan <affanbhuiyan.project@gmail.com>
2026-09-13 20:28:17 +0600

    Set up React project

---
+++ src/components/Nav.tsx
@@ -0,0 +1,33 @@
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+
+        {/* Logo */}
+        <div className="logo">
+          <img src="/logo-text.png" alt="Dev Stack" />
+        </div>
+
+        {/* Navigation */}
+        <div className="nav-links">
+          <a href="#" className="active">Home</a>
+          <a href="#">Technologies</a>
+          <a href="#">Projects</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+
+        {/* Buttons */}
+        <div className="nav-buttons">
+          <button className="sign-in">Sign In</button>
+          <button className="sign-up">Sign Up</button>
+        </div>
+
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
