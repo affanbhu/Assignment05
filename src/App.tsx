f98376e4bda386c4c095a7d9652ebd75d2c8f883
AffanBhuiyan <affanbhuiyan.project@gmail.com>
2026-09-13 21:00:33 +0600

    Describe your changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
+import TechnologySection from "./components/TechnologySection";
 
 function App() {
   return (
     <>
       <Nav />
+
       <Hero />
+
+      <TechnologySection />
     </>
   );
 }
